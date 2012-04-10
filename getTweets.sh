41d6de55a077fe838133b1fdcc95876ee426b92b
Roland Tanglao <roland@rolandtanglao.com>
2012-04-09 20:23:22 -0700

    add getTweets.sh a helper script to get all the initial users tweets

---
+++ getTweets.sh
@@ -0,0 +1,11 @@
+./getLast3200Tweets.rb translink 2> getlast3200.translink.stderr.out
+./getLast3200Tweets.rb travelsmart 2> getlast3200.travelsmart.stderr.out
+./getLast3200Tweets.rb greenestcity 2> getlast3200.greenestcity.stderr.out
+./getLast3200Tweets.rb bepartoftheplan 2> getlast3200.bepartoftheplan.stderr.out
+./getLast3200Tweets.rb skytrainlady 2> getlast3200.skytrainlady.stderr.out
+./getLast3200Tweets.rb vantransp2040 2> getlast3200.vantransp2040.stderr.out
+./getLast3200Tweets.rb covfoodpolicy 2> getlast3200.covfoodpolicy.stderr.out
+./getLast3200Tweets.rb vansocialpolicy 2> getlast3200.vansocialpolicy.stderr.out
+./getLast3200Tweets.rb viva_van 2> getlast3200.viva_van.stderr.out
+./getLast3200Tweets.rb vangreenbldgs 2> getlast3200.vangreenbldgs.stderr.out
+
